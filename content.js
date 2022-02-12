5834fb9567d83e962a7b2165a3fdd70bbbe055eb
Rodewitsch <rodewitsch@inbox.ru>
2022-02-12 22:51:30 +0300

    feat: get image from background-image

--- content.js
+++ content.js
@@ -7,40 +7,40 @@ document.addEventListener("contextmenu", function (event) {
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request == "getClickedEl") {
-    (async function(){
-      const image = findNearestImage(clickedEl);
-      if (!image) alert('No image found');
+    (async function () {
+      const imageSrc = findNearestImageSrc(clickedEl);
+      if (!imageSrc) alert('No image found');
 
-      const base64 = await getBase64ImageFromElement(image);
+      const base64 = await getBase64ImageFromImageSrc(imageSrc);
       return sendResponse({ base64 });
     })();
   }
   return true;
 });
 
-function findNearestImage(element) {
-  if (element.nodeName.toLowerCase() === "img") return element;
-  const parentElement = element.parentElement;
-  if (parentElement.nodeName.toLowerCase() === "img") return parentElement;
+function findNearestImageSrc(element) {
+  if (!element) return null;
+  if (element.nodeName.toLowerCase() === "img") return element.currentSrc;
+  const parentElement = element.parentNode;
+  if (parentElement.nodeName.toLowerCase() === "img") return parentElement.currentSrc;
   const parentImage = parentElement.querySelector('img');
-  if (parentImage) return parentImage;
-  return findNearestImage(parentElement);
+  if (parentImage) return parentImage.currentSrc;
+  const elementStyles = element.currentStyle || window.getComputedStyle(element, false);
+  if (elementStyles.backgroundImage && elementStyles.backgroundImage !== "none") {
+    return elementStyles.backgroundImage.slice(4, -1).replace(/"/g, "");
+  }
+  return findNearestImageSrc(parentElement);
 }
 
-async function getBase64ImageFromElement(element) {
-  if (element) {
-    if (element.currentSrc) {
-      if (isBase64(element.currentSrc, { allowMime: true })) return element.currentSrc;
-      // const image = await toDataURL(element.currentSrc);
-      const response = await browser.runtime.sendMessage({ type: 'getBase64ImageFromElement', src: element.currentSrc });
-      return response.base64;
-    }
-  }
+async function getBase64ImageFromImageSrc(src) {
+  if (isBase64(src, { allowMime: true })) return src;
+  const response = await browser.runtime.sendMessage({ type: 'getBase64ImageFromElement', src });
+  return response.base64;
 }
 
 function toDataURL(src, outputFormat) {
   return new Promise((resolve, reject) => {
-    
+
     var img = new Image();
     img.crossOrigin = 'Anonymous';
     img.onload = function () {
