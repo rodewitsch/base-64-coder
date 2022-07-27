7dbf949b08f9d826f47508786921339908f9ff0c
Rodewitsch <rodewitsch@inbox.ru>
2022-07-27 21:15:12 +0300

    feat: input file

--- convert/script.js
+++ convert/script.js
@@ -8,11 +8,24 @@ const copyToClipboard = value => {
   return Promise.reject('The Clipboard API is not available.');
 };
 
-const pasteFromClipboard = () => {
+function pasteFromClipboard() {
   if (navigator && navigator.clipboard && navigator.clipboard.readText) {
     return navigator.clipboard.readText();
   }
   return Promise.reject('The Clipboard API is not available.');
+}
+
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      return resolve(reader.result);
+    };
+    reader.onerror = function (error) {
+      return reject(error);
+    };
+  });
 }
 
 document.onreadystatechange = function () {
@@ -22,6 +35,7 @@ document.onreadystatechange = function () {
   const resultAudio = document.getElementById('result-audio');
   const resultVideo = document.getElementById('result-video');
   const resultVideoSource = document.getElementById('result-video-source');
+  const openSourceFile = document.getElementById('open-source-file');
   const encodeBtn = document.getElementById('encode-btn');
   const decodeBtn = document.getElementById('decode-btn');
   const decodeImage = document.getElementById('decode-image-btn');
@@ -54,7 +68,23 @@ document.onreadystatechange = function () {
   resultAudio.style.display = 'none';
   resultVideo.style.display = 'none';
 
-  encodeBtn.onclick = () => {
+  openSourceFile.onclick = () => {
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = async () => {
+      let files = Array.from(input.files);
+      if (files.length > 0) {
+        const base64 = await getBase64(files[0]);
+        result.value = base64.replace('data:text/plain;base64,', '');
+      }
+    };
+    input.click();
+  }
+
+  encodeBtn.onclick = (event) => {
+    if (event.currentTarget.classList.contains('disabled')) {
+      return;
+    }
     let encodedText;
     const text = source.value;
     try {
@@ -72,10 +102,17 @@ document.onreadystatechange = function () {
     copyResult.classList.remove('disabled');
   };
 
-  decodeBtn.onclick = () => {
+  decodeBtn.onclick = (event) => {
+    if (event.currentTarget.classList.contains('disabled')) {
+      return;
+    }
     const base64 = source.value;
-    const decodedText = decodeURIComponent(atob(base64));
-    result.value = decodedText;
+    try {
+      result.value = decodeURIComponent(atob(base64));
+    }
+    catch (err) {
+      result.value = atob(base64);
+    }
     result.style.display = 'block';
     resultImg.style.display = 'none';
     resultAudio.style.display = 'none';
@@ -85,7 +122,10 @@ document.onreadystatechange = function () {
     copyResult.classList.remove('disabled');
   };
 
-  decodeImage.onclick = () => {
+  decodeImage.onclick = (event) => {
+    if (event.currentTarget.classList.contains('disabled')) {
+      return;
+    }
     const base64 = source.value.replace(/data:image\/.+?;base64,/, '');
     resultImg.src = `data:image/jpeg;base64,${base64}`;
     result.style.display = 'none';
@@ -97,7 +137,10 @@ document.onreadystatechange = function () {
     copyResult.classList.add('disabled');
   };
 
-  decodeAudio.onclick = () => {
+  decodeAudio.onclick = (event) => {
+    if (event.currentTarget.classList.contains('disabled')) {
+      return;
+    }
     const base64 = source.value.replace(/data:audio\/.+?;base64,/, '');
     resultAudio.src = `data:audio/mp3;base64,${base64}`;
     result.style.display = 'none';
@@ -108,7 +151,10 @@ document.onreadystatechange = function () {
     copyResult.classList.add('disabled');
   };
 
-  decodeVideo.onclick = () => {
+  decodeVideo.onclick = (event) => {
+    if (event.currentTarget.classList.contains('disabled')) {
+      return;
+    }
     const base64 = source.value.replace(/data:video\/.+?;base64,/, '');
     resultVideoSource.src = `data:video/mp4;base64,${base64}`;
     result.style.display = 'none';
@@ -120,16 +166,39 @@ document.onreadystatechange = function () {
     copyResult.classList.add('disabled');
   };
 
+  source.onkeyup = (event) => {
+    if (event.target.value) {
+      decodeBtn.classList.remove('disabled');
+      encodeBtn.classList.remove('disabled');
+      decodeImage.classList.remove('disabled');
+      decodeAudio.classList.remove('disabled');
+      decodeVideo.classList.remove('disabled');
+    } else {
+      decodeBtn.classList.add('disabled');
+      encodeBtn.classList.add('disabled');
+      decodeImage.classList.add('disabled');
+      decodeAudio.classList.add('disabled');
+      decodeVideo.classList.add('disabled');
+    }
+  };
+
   copySource.onclick = () => copyToClipboard(source.value);
 
   pasteSource.onclick = async () => {
     const text = await pasteFromClipboard();
     source.value = text;
+    source.dispatchEvent(new Event('keyup'));
   };
 
-  clearSource.onclick = () => source.value = '';
+  clearSource.onclick = () => {
+    source.value = '';
+    source.dispatchEvent(new Event('keyup'));
+  }
 
-  clearResult.onclick = () => {
+  clearResult.onclick = (event) => {
+    if (event.currentTarget.classList.contains('disabled')) {
+      return;
+    }
     result.value = '';
     resultImg.src = null;
     resultAudio.src = null;
@@ -142,7 +211,10 @@ document.onreadystatechange = function () {
     resultType = 'text';
   }
 
-  saveResult.onclick = () => {
+  saveResult.onclick = (event) => {
+    if (event.currentTarget.classList.contains('disabled')) {
+      return;
+    }
     if (resultType === 'text') {
       const blob = new Blob([result.value], { type: 'text/plain' });
       saveAs(blob, "text.txt");
@@ -158,7 +230,10 @@ document.onreadystatechange = function () {
     }
   };
 
-  copyResult.onclick = () => {
+  copyResult.onclick = (event) => {
+    if (event.currentTarget.classList.contains('disabled')) {
+      return;
+    }
     if (resultType === 'text') {
       copyToClipboard(result.value);
     }
