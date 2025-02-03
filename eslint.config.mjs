eae62a84f863f63d52af9ab2346f8fb83ce15579
Rodewitsch <rodewitsch@inbox.ru>
2025-02-03 19:45:17 +0300

    build: enable eslint
    #26

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.js"], languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.webextensions,
+        ...globals.serviceworker,
+        copyToClipboard: true,
+        pasteFromClipboard: true,
+        getBase64: true,
+        base64UrlEncode: true,
+        base64UrlDecode: true,
+        isJWT: true,
+        isBase64: true,
+        isJSON: true,
+        saveAs: true,
+        parseJwt: true,
+      }, sourceType: "script"
+    }
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+];
